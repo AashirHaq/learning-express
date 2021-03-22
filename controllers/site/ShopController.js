606d89fdb68a63001e68d3b4130412b6570c865e
aashirhaq <aashihaque@gmail.com>
2021-03-22 13:04:40 +0500

    07 The Model View Controller (MVC) DONE

--- controllers/site/ShopController.js
+++ controllers/site/ShopController.js
@@ -1,13 +1,14 @@
 const Product = require('../../models/Product');
 
 exports.index = (req, res, next) => {
-    const products = Product.list();
-    res.render('shop', {
-        prods: products,
-        docTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCss: true
+    Product.list(products => {
+        res.render('shop', {
+            prods: products,
+            docTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCss: true
+        })
     })
 }
--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,20 @@
-const records = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename), 
+    'storage', 
+    'products.json'
+);
+
+const recordsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err){
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +22,15 @@ module.exports = class Product {
     }
 
     save() {
-        records.push(this);
+        recordsFromFile(records => {
+            records.push(this);
+            fs.writeFile(p, JSON.stringify(records), (err) => {
+                console.log(err);
+            })
+        })
     }
 
-    static list() {
-        return records;
+    static list(cb) {
+        recordsFromFile(cb)
     }
 }
